75e6a22067f229496b00049704b29789d94aa046
saikuru0 <me@saikuru.net>
2024-12-23 17:04:49 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use std::fs::File;
+use clap::Parser;
+use csv::{ReaderBuilder, WriterBuilder};
+
+#[derive(Parser, Debug)]
+#[command(version, about = "CSV cleanup and reformat tool", long_about = None)]
+struct Args {
+    /// Input CSV filepath
+    #[arg(short, long)]
+    input: String,
+
+    /// Output CSV filepath
+    #[arg(short, long)]
+    output: String,
+
+    /// Delimiter to use
+    #[arg(short, long, default_value = ",")]
+    delimiter: char,
+
+    /// Escape with backslash instead of double-quoting
+    #[arg(short, long)]
+    escape: bool,
+
+    /// Flexible read and write (doesn't skip rows with column count mismatch)
+    #[arg(short, long)]
+    flexible: bool,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let in_file = File::open(&args.input)?;
+    let mut reader = ReaderBuilder::new()
+        .has_headers(false)
+        .flexible(args.flexible)
+        .delimiter(args.delimiter as u8)
+        .from_reader(in_file);
+
+    let out_file = File::create(&args.output)?;
+    let mut writer = WriterBuilder::new()
+        .flexible(args.flexible)
+        .quote_style(csv::QuoteStyle::Always)
+        .double_quote(!(args.escape))
+        .from_writer(out_file);
+
+    for result in reader.records() {
+        let record = result?;
+        let _ = writer.write_record(&record);
+    }
+
+    writer.flush()?;
+    Ok(())
+}
